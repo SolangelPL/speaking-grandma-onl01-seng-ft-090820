101499f874d8ccc407e21f5604d8d0cf5abb5069
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-11 19:05:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,5 @@ def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
      return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.upcase && phrase = "I LOVE YOU GRANDMA!"
-        
   end
 end
